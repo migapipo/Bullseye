a1b9f57b65577a9431e7596883fcf7ff48370b7a
migapipo <670475225@qq.com>
2022-03-07 00:02:07 +1100

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Xiaoyu Sun on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
