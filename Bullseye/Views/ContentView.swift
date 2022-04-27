b4bb421edfb1ff8f1c96f64e0d17d27af4cfab7f
migapipo <670475225@qq.com>
2022-04-28 09:43:10 +1000

    created a data model

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     
     @State private var alertIsVisible : Bool = false
     @State private var sliderValue: Double = 50.0
+    @State private var game: Game = Game()
     
     var body: some View {
         VStack {
@@ -21,7 +22,7 @@ struct ContentView: View {
                     .multilineTextAlignment(.center)
                     .lineSpacing(4.0)
                     .font(.footnote)
-                Text("89")
+                Text(String(game.target))
                     .kerning(-1.0)
                     .font(.largeTitle)
                     .fontWeight(.black)
@@ -39,7 +40,7 @@ struct ContentView: View {
                 Text("Hit me")
             }.alert(isPresented: $alertIsVisible, content: {
                 var roundedValue: Int = Int(self.sliderValue.rounded())
-                return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(roundedValue)"), dismissButton: .default(Text("Back")))
+                return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(roundedValue).\n" + "Your scored \(self.game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Back")))
             })
         }
       
