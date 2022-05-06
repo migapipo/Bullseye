27bf6e7d08ad43aff956aa52503114566ac04639
migapipo <670475225@qq.com>
2022-05-06 22:34:40 +1000

    add test cases to implement test-driven development

---
+++ BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,37 @@
+//
+//  BullseyeTests.swift
+//  BullseyeTests
+//
+//  Created by Xiaoyu Sun on 6/5/22.
+//
+
+import XCTest
+@testable import Bullseye
+
+class BullseyeTests: XCTestCase {
+
+    var game: Game!
+    override func setUpWithError() throws {
+        game = Game()
+    }
+
+    override func tearDownWithError() throws {
+       game = nil
+    }
+
+    func testExample() throws {
+        XCTAssertEqual(game.points(sliderValue: 50), 999)
+    }
+
+    func testScorePositive(){
+        var guess = game.target + 5
+        var score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+    }
+    
+    func testScoreNegative(){
+        var guess = game.target - 5
+        var score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+    }
+}
